11e7604fd5796414f9963953f7d7d4a13e543db6
Nethsara-code <hirushanethsara59@gmail.com>
2026-01-24 01:03:02 +0530

    Bike class upadate

--- src/Bike.java
+++ src/Bike.java
@@ -1,2 +1,17 @@
-public class Bike {
+public class Bike extends Vehicle{
+
+    private int engineCapacityCC;
+
+    public Bike(String id, String brand, String model, double rate, int cc){
+        super(id, brand, model, rate);
+        this.engineCapacityCC = cc;
+    }
+
+    public double  calculateRentalCost(int days){
+        return getBaseRatePerDay() * days + (engineCapacityCC * 0.5 * days);
+    }
+
+
 }
+
+
--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,12 @@
-public class Car {
+public class Car extends Vehicle{
+    private int numberOfSeats;
+
+    public  Car(String id, String brand, String model, double rate, int seats ){
+        super(id, brand, model, rate);
+        this.numberOfSeats = seats;
+    }
+
+    public double calculateRentalCost(int days){
+        return getBaseRatePerDay() * days + (numberOfSeats * 2000 * days);
+    }
 }
