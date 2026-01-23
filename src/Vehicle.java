9ab435e443e9d862e02ff4783a83af31a14a9f02
Nethsara-code <hirushanethsara59@gmail.com>
2026-01-23 23:51:24 +0530

    vehicle class update

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,5 +2,9 @@ public abstract class vehicle{
 
     private String vehicle;
     private String brand;
+    private  String model;
+    private double baseRatePerDay;
+    private boolean isAvailable;
     
+
 }
