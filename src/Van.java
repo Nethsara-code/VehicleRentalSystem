0192e1cf2e7d1d4ebefd94c25564235390d876e4
Nethsara-code <hirushanethsara59@gmail.com>
2026-01-24 01:07:32 +0530

    Van class update

--- src/Van.java
+++ src/Van.java
@@ -1,2 +1,14 @@
-public class Van {
+public class Van extends Vehicle {
+
+    private double cargoCapacityKg;
+
+    public Van(String id, String brand, String model, double rate, double cargoKg) {
+        super(id, brand, model, rate);
+        this.cargoCapacityKg = cargoKg;
+    }
+
+    public double calculateRentalCost(int days) {
+        return getBaseRatePerDay() * days + (cargoCapacityKg * 0.2 * days);
+    }
+
 }
