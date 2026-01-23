c6225bd46c726a5edfa73b22ab3af419dc20190a
Nethsara-code <hirushanethsara59@gmail.com>
2026-01-24 00:25:07 +0530

    class update

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -1,10 +1,58 @@
-public abstract class vehicle{
+public abstract class Vehicle{
 
-    private String vehicle;
+    private String vehicleId;
     private String brand;
     private  String model;
     private double baseRatePerDay;
     private boolean isAvailable;
-    
+
+    public Vehicle(String Vehicle, String brand, String model, double baseRatePerDay){
+        this.vehicleId = vehicleId;
+        this.brand = brand;
+        this.model = model;
+        this.baseRatePerDay = baseRatePerDay;
+        this.isAvailable = isAvailable = true;
+    }
+
+    public String getVehicleId(){
+        return vehicleId;
+    }
+
+    public String getBrand(){
+        return brand;
+    }
+
+    public String getModel(){
+        return model;
+    }
+
+    public double getBaseRatePerDay(){
+        return baseRatePerDay;
+    }
+
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+
+    public void rentVehicle(){
+        isAvailable = false;
+    }
+
+    public void returnVehicle(){
+        isAvailable = true;
+    }
+
+    public void displayDetails(){
+        System.out.println("ID : "+vehicleId+
+                "Brand : " + brand+
+                "Model : " + model+
+                "Rate : "+ baseRatePerDay+
+                "Available : " + isAvailable
+
+                );
+    }
+
+    public abstract double calculateRentalCost(int days);
+
 
 }
